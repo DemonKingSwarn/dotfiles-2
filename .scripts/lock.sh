681e4a92294eecb88baafece0cfe5fc79764301b
demonkingswarn <rockingswarn@gmail.com>
2022-01-10 21:07:19 +0530

    autocommit

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -1,18 +1,8 @@
-#!/usr/bin/env bash
-
-# set the icon and a temporary location for the screenshot to be stored
-icon="$HOME/.config/i3/lock.png"
-tmpbg='/tmp/screen.png'
-
-# take a screenshot
-flameshot -c "$tmpbg"
-
-# blur the screenshot by resizing and scaling back up
-convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
-
-# overlay the icon onto the screenshot
-convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
-
-# lock the screen with the blurred screenshot
-i3lock -i "$tmpbg"
-
+#!/bin/bash
+ICON=$HOME/.config/i3/lock.png
+IMAGEDIR=/tmp/uwu.png
+scrot $IMAGEDIR
+convert $IMAGEDIR -scale 10% -scale 1000% $IMAGEDIR
+convert $IMAGEDIR $ICON -gravity center -composite -matte $IMAGEDIR
+i3lock -u -i $IMAGEDIR
+rm -rf /tmp/uwu.png

196bcf51fff3d22cd2d0d331aeaf7a7949e46a9d
DemonKingSwarn <rockingswarn@gmail.com>
2022-01-30 20:35:14 +0530

    autocommit

--- .scripts/autoRicer.sh
+++ .scripts/autoRicer.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
-# build essential
 apt-get install build-essential -y
-
-# apt-fast
 add-apt-repository ppa:apt-fast/stable
 apt-get update
 apt-get -y install apt-fast
-
-# brew - un comment it if you really want brew.
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# i3wm
 apt-fast install i3 -y
-
-# i3gaps and required dependencies for i3gaps
 apt-fast install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake -y
-
 mkdir tmp
 cd tmp
 git clone https://github.com/Airblader/xcb-util-xrm
@@ -25,7 +14,6 @@ git submodule update --init
 ./autogen.sh --prefix=/usr
 make
 sudo make install
-
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
@@ -36,8 +24,6 @@ cd build
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
 sudo make install
-
-# auto rices i3
 cd
 git clone https://github.com/demonkingswarn/dotfiles-2.git
 rm -rf .config/i3
